c46c2f231e68d02221a76d2788acc19b24ee8981
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-06-30 13:16:15 +0530

    [Soubarnika]Rolling Die UC2

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -7,6 +7,7 @@ namespace SnakeAndLadder
         /// <summary>
         /// Snake and Ladder computation
         /// UC1-Initialize player and Initial position
+        /// UC2- Generating Die Roll using Random class
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
@@ -14,10 +15,17 @@ namespace SnakeAndLadder
             //initializing local variable where no. of player is 1 and initial position is 0
             int player = 1;
             int playerPos = 0;
+            //creating object for Random class
+            Random random = new Random();
+
+            //Generating DieRoll using Next method
+            int dieRoll = random.Next(1, 7);
 
             Console.WriteLine("Welcome to Snake and Ladder World");
-            Console.WriteLine("Player " + player + " is in position " + playerPos + " initially");
+            Console.WriteLine("Player " + player + " is in position " + playerPos + " got number " + dieRoll);
             Console.Read();
         }
+
+
     }
 }
