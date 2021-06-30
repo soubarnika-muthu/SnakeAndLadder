7f201c2a61b32727590918187801f92e1b1df1e8
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-06-30 09:58:51 +0530

    [Soubarnika]Intializing player and initial position

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,9 +4,20 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        /// <summary>
+        /// Snake and Ladder computation
+        /// UC1-Initialize player and Initial position
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //initializing local variable where no. of player is 1 and initial position is 0
+            int player = 1;
+            int playerPos = 0;
+
+            Console.WriteLine("Welcome to Snake and Ladder World");
+            Console.WriteLine("Player " + player + " is in position " + playerPos + " initially");
+            Console.Read();
         }
     }
 }
